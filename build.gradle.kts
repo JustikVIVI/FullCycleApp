99d18d0e1d060d46269e21ce03466eba1415ba07
ilia <ilia@Maries-Air.fritz.box>
2024-05-23 10:53:30 +0200

    OpenApi

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,26 +1,51 @@
 plugins {
-	java
-	id("org.springframework.boot") version "3.2.5"
-	id("io.spring.dependency-management") version "1.1.4"
+    java
+    id("org.springframework.boot") version "3.2.5"
+    id("io.spring.dependency-management") version "1.1.4"
+    id("org.openapi.generator") version "7.5.0"
+}
+
+// generated sources of openapi generate task
+val generatedSourcesDir = "$buildDir/generated/openapi"
+sourceSets.getByName("main").java {
+    srcDir("$generatedSourcesDir/src/main/java")
 }
 
 group = "com.learning"
 version = "0.0.1-SNAPSHOT"
 
 java {
-	sourceCompatibility = JavaVersion.VERSION_17
+    sourceCompatibility = JavaVersion.VERSION_17
 }
 
 repositories {
-	mavenCentral()
+    mavenCentral()
 }
 
 dependencies {
-	implementation("org.springframework.boot:spring-boot-starter-web")
-	testImplementation("org.springframework.boot:spring-boot-starter-test")
-	testRuntimeOnly("org.junit.platform:junit-platform-launcher")
+    implementation("org.springdoc:springdoc-openapi-ui:1.6.14")
+    implementation("org.springframework.boot:spring-boot-starter-web")
+    implementation("org.springframework.boot:spring-boot-starter-validation")
+    implementation("javax.validation:validation-api:2.0.1.Final")
+    implementation("javax.annotation:javax.annotation-api:1.3.2")
+    implementation("org.openapitools:jackson-databind-nullable:0.2.6")
+    implementation("javax.servlet:javax.servlet-api:3.0.1")
+
+    testImplementation("org.springframework.boot:spring-boot-starter-test")
+    testRuntimeOnly("org.junit.platform:junit-platform-launcher")
 }
 
 tasks.withType<Test> {
-	useJUnitPlatform()
+    useJUnitPlatform()
+}
+
+openApiGenerate {
+    generatorName.set("spring") // Choose the generator (e.g., "spring", "java", etc.)
+    inputSpec.set("$rootDir/src/main/resources/api.yaml") // Path to your OpenAPI spec
+    outputDir.set(generatedSourcesDir)
+    configOptions.set(
+        mapOf(
+            "interfaceOnly" to "true" // Generate only interfaces
+        )
+    )
 }
--- src/main/java/com/learning/demo/HelloWorldController.java
+++ src/main/java/com/learning/demo/HelloWorldController.java
@@ -1,2 +1,18 @@
-package com.learning.demo;public class HelloWorldController {
+package com.learning.demo;
+
+import org.openapitools.api.HelloApi;
+import org.openapitools.model.HelloWorldApiResponse;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Controller;
+
+@Controller
+public class HelloWorldController implements HelloApi {
+
+    @Override
+    public ResponseEntity<HelloWorldApiResponse> getHello() {
+        HelloWorldApiResponse response = new HelloWorldApiResponse();
+        response.value("Helloworld");
+
+        return ResponseEntity.ok(response);
+    }
 }
