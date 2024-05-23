6cecd2a2bcec5f5fce1cb91d12abfce5e56e1da1
ilia <ilsosnin@gmail.com>
2024-05-23 11:09:03 +0200

    Updated dummy test

--- src/test/java/com/learning/demo/DemoApplicationTests.java
+++ src/test/java/com/learning/demo/DemoApplicationTests.java
@@ -3,11 +3,16 @@ package com.learning.demo;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
 
+import static org.junit.jupiter.api.Assertions.*;
+
 @SpringBootTest
 class DemoApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void simpleTest() {
+		int a = 2 + 2;
+
+		assertEquals(4, a);
 	}
 
 }
