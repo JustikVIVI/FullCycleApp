ed8e01bddecdb6a3a7c80feb143100e98c597cf6
ilia <ilsosnin@gmail.com>
2024-05-26 12:15:10 +0200

    Fixed issues

--- src/main/java/com/learning/demo/SendController.java
+++ src/main/java/com/learning/demo/SendController.java
@@ -1,21 +1,17 @@
 package com.learning.demo;
 
-import org.openapitools.api.HelloApi;
 import org.openapitools.api.SendApi;
 import org.openapitools.model.HelloWorldApiResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 
 @Controller
-public class SendController implements SendApi {
+public final class SendController implements SendApi {
 
     @Override
-    // TODO: remove that
     public ResponseEntity<HelloWorldApiResponse> getMeme() {
         HelloWorldApiResponse response = new HelloWorldApiResponse();
         response.setValue("SEND NUDES");
-        response = null;
-        response.setValue("hello");
         return ResponseEntity.ok(response);
     }
 }
